d5dd256c59d031b3715d95c2bf3dd6c1dc4aab54
Hurricane <74684060+Trvvis@users.noreply.github.com>
2023-08-13 20:40:47 -0600

    Create geohack.js

---
+++ geohack.js
@@ -0,0 +1,13 @@
+onst linkElement = document.querySelector('a[href*="https://maps.google.com/maps/"]');
+
+const href = linkElement.getAttribute('href');
+
+const regex = /@(-?\d+\.\d+),(-?\d+\.\d+)/;
+const match = href.match(regex);
+const latitude = match[1];
+const longitude = match[2];
+
+console.log(`${latitude},${longitude}`);
+console.log(`${href}`);
+
+// Made by Hurricane, if you share this on YouTube or anything PLEASE CREDIT ME!!!!!!!!
